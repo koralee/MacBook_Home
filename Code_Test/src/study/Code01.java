7afed1a340b530e041cc489778441a5e420ba029
Suhong <honge@localhost>
2022-10-29 22:49:50 +0900

    3,6,9 game practice

--- Code_Test/src/study/Code01.java
+++ Code_Test/src/study/Code01.java
@@ -13,14 +13,17 @@ public class Code01 {
 		
 		if(num>0&& num<100) {
 			System.out.println(num);
-			if(num>=30&&(num-(num%3))%30==0) {
-				System.out.print("박수 짝!");;
-			if(num%3==3) {
-					System.out.println("짝!");
+			
+				if(num>=30 && (num-(num%10))%30==0) { 
+				System.out.print("박수 짝!");
+						if(num%10!=0 && num%3==0)
+						System.out.println("짝!");
 				}
-			}
-			else if(num<30 && (num-num%10)%3==0)
-				System.out.println("박수 짝!");
+			
+				else if(num%10!=0 && (num%10)%3==0)
+					System.out.println("박수 짝!");
+				else
+					System.out.println("박수X");
 		}
 		else
 			System.out.println("범위를 잘못 입력하였습니다.");

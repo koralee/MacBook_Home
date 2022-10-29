af2660344e888e7d2ceaee827d1b6028b2f83c91
Suhong <honge@localhost>
2022-10-29 12:49:37 +0900

    369

--- Code_Test/src/study/Code01.java
+++ Code_Test/src/study/Code01.java
@@ -1,12 +1,29 @@
 package study;
 
+import java.util.*;
+
 public class Code01 {
 
 	public static void main(String[] args) {
-		System.out.println("ㅎㅎ");
-		System.out.println("ㅠㅠ");
+		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("좀 돼라!!!");
+		System.out.println("369게임");
+		System.out.println("1~99의 숫자를 입력하시오");
+		int num = sc.nextInt();
+		
+		if(num>0&& num<100) {
+			System.out.println(num);
+			if(num>=30&&(num-(num%3))%30==0) {
+				System.out.print("박수 짝!");;
+			if(num%3==3) {
+					System.out.println("짝!");
+				}
+			}
+			else if(num<30 && (num-num%10)%3==0)
+				System.out.println("박수 짝!");
+		}
+		else
+			System.out.println("범위를 잘못 입력하였습니다.");
 		
 	}
 
